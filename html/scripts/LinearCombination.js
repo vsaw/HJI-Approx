6785672db91b0afb7f169129d0dd1c65e728d411
Valentin Sawadski <valentin@sawadski.eu>
2013-09-01 16:09:45 +0200

    Added documentation.

--- html/scripts/LinearCombination.js
+++ html/scripts/LinearCombination.js
@@ -34,9 +34,23 @@
 
 var Definitions = require('./Definitions.js');
 
+/**
+ * Represents a linear combination of a point x such that
+ * 
+ * x = factor0 * point0 + factor1 * point1 + ... + factorN * pointN
+ * 
+ * Please note that this class will mostly be used for convex combinations.
+ * Although the factors are not scanned to ensure a convex combination.
+ */
 var LinearCombination = function() {
 
+	/**
+	 * An array containing the factors of the linear combination.
+	 */
 	var factors = [];
+	/**
+	 * An array of states being the points of the linear combination.
+	 */
 	var points = [];
 
 	for ( var i = 0; i < Definitions.LINEAR_COMBINATON_LENGTH; i++) {
@@ -44,6 +58,16 @@ var LinearCombination = function() {
 		points[i] = null;
 	}
 
+	/**
+	 * Evaluates the linear combination and returns the target it is pointing
+	 * to.
+	 * 
+	 * This will evaluate the following equation and return the variable x
+	 * 
+	 * x = factor0 * point0 + factor1 * point1 + ... + factorN * pointN
+	 * 
+	 * @returns The target of the linear combination.
+	 */
 	this.eval = function() {
 		var ret = [];
 		for ( var i = 0; i < Definitions.STATE_SPACE_DIMENSION; i++) {
@@ -62,18 +86,70 @@ var LinearCombination = function() {
 		return ret;
 	};
 
+	/**
+	 * Returns the i-th factor.
+	 * 
+	 * @param i
+	 *            The index. Must be 0 <= i <= LINEAR_COMBINATION_LENGTH
+	 * 
+	 * @returns The i-th factor.
+	 * 
+	 * @throws EXCEPTION_INDEX_OUT_OF_BOUNDS
+	 *             i was not a valid index. Must be 0 <= i <=
+	 *             LINEAR_COMBINATION_LENGTH
+	 */
 	this.getFactor = function(index) {
 		return factors[index];
 	};
 
+	/**
+	 * Returns the i-th point.
+	 * 
+	 * The point might be null if not used in the linear combination. However it
+	 * might still be possible that an point with an index larger than "i" is
+	 * set.
+	 * 
+	 * @param i
+	 *            The index. Must be 0 <= i <= LINEAR_COMBINATION_LENGTH
+	 * 
+	 * @returns The i-th point.
+	 * 
+	 * @throws EXCEPTION_INDEX_OUT_OF_BOUNDS
+	 *             i was not a valid index. Must be 0 <= i <=
+	 *             LINEAR_COMBINATION_LENGTH
+	 */
 	this.getPoint = function(index) {
 		return points[index];
 	};
 
+	/**
+	 * Sets the i-th factor of the linear combination.
+	 * 
+	 * @param i
+	 *            The index
+	 * @param f
+	 *            the new value.
+	 * 
+	 * @throws EXCEPTION_INDEX_OUT_OF_BOUNDS
+	 *             i was not a valid index. Must be 0 <= i <=
+	 *             LINEAR_COMBINATION_LENGTH
+	 */
 	this.setFactor = function(index, factor) {
 		factors[index] = factor;
 	};
 
+	/**
+	 * Sets the i-th point of the linear combination.
+	 * 
+	 * @param i
+	 *            The index
+	 * @param x
+	 *            The new point.
+	 * 
+	 * @throws EXCEPTION_INDEX_OUT_OF_BOUNDS
+	 *             i was not a valid index. Must be 0 <= i <=
+	 *             LINEAR_COMBINATION_LENGTH
+	 */
 	this.setPoint = function(index, point) {
 		points[index] = point;
 	};
